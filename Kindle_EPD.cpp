570a8f056f5c82546c370db26cb869f83b38c45b
dnbattley <dave.battley@gmail.com>
2025-03-13 15:19:11 +1000

    Update Kindle_EPD.cpp

--- Kindle_EPD.cpp
+++ Kindle_EPD.cpp
@@ -3,10 +3,10 @@
 // Constants
 #define EPD_WHITE 0xFF
 #define EPD_BLACK 0x00
-#define EPD_UPDATE_FULL 0
-#define EPD_UPDATE_FAST 1
-#define EPD_UPDATE_PARTIAL 2
-#define EPD_GRAY_LEVELS 4
+#define UPDATE_FULL 0
+#define UPDATE_FAST 1
+#define UPDATE_PARTIAL 2
+#define GRAY_LEVELS 4
 
 #define RESET_DELAY 10
 #define POWER_ON_DELAY 300
@@ -23,33 +23,47 @@ void SPI_Write(unsigned char value)
 }
 
 // SPI write command
-void EPD_W21_WriteCMD(unsigned char command)
+void WriteCmd(unsigned char command, int screen)
 {
-    EPD_W21_CS_0;
-    EPD_W21_DC_0;  // D/C#   0:command  1:data  
+    if(screen == 1) {
+        screen1_CS_0;
+        screen1_DC_0; 
+    } else {
+        screen2_CS_0;
+        screen2_DC_0;
+    }
     SPI_Write(command);
-    EPD_W21_CS_1;
+    if(screen == 1) {
+        screen1_CS_1;
+    } else {
+        screen2_CS_1;
+    }
 }
 
 // SPI write data
-void EPD_W21_WriteDATA(unsigned char data)
+void WriteData(unsigned char data, int screen)
 {
-    EPD_W21_CS_0;
-    EPD_W21_DC_1;  // D/C#   0:command  1:data
+    if(screen == 1) {
+        screen1_CS_0;
+        screen1_DC_1;
+    } else {
+        screen2_CS_0;
+        screen2_DC_1;
+    }
     SPI_Write(data);
-    EPD_W21_CS_1;
+    if(screen == 1) {
+        screen1_CS_1;
+    } else {
+        screen2_CS_1;
+    }
 }
 
 //////////////////////////////////////////////////////
 
-// Global Variables
-unsigned char partImage[1000]; // Partial canvas space
-
 // Helper Functions
-void epd_wait_busy(void) {
-    // Implement a timeout mechanism to avoid potential infinite loops
+void waitBusy(int screen) {
     unsigned long start = millis();
-    while (isEPD_W21_BUSY == 0) {
+    while ((screen == 1 ? isScreen1_BUSY : isScreen2_BUSY) == 0) {
         if (millis() - start > 5000) { // Timeout after 5 seconds
             Serial.println("Error: EPD busy timeout");
             break;
@@ -57,51 +71,59 @@ void epd_wait_busy(void) {
     }
 }
 
-void epd_reset(void) {
-    EPD_W21_RST_0;
+void resetDisplay(int screen) {
+    if(screen == 1) {
+        screen1_RST_0;
+    } else {
+        screen2_RST_0;
+    }
     delay(RESET_DELAY);
-    EPD_W21_RST_1;
+    if(screen == 1) {
+        screen1_RST_1;
+    } else {
+        screen2_RST_1;
+    }
     delay(RESET_DELAY);
 }
 
 // Initialization
-void epd_init(int mode) {
-    epd_reset();
-    EPD_W21_WriteCMD(0x00);
-    EPD_W21_WriteDATA(0x1F); // EPD_W21_WriteDATA(0x13) here for 180 deg
-    EPD_W21_WriteCMD(0x04); // POWER ON
+void initDisplay(int mode, int screen) {
+    resetDisplay(screen);
+    WriteCmd(0x00, screen);
+    WriteData(0x1F, screen); // WriteData(0x13) here for 180 deg
+    WriteCmd(0x04, screen); // POWER ON
     delay(POWER_ON_DELAY);
-    epd_wait_busy();
+    waitBusy(screen);
 
     switch (mode) {
-        case EPD_UPDATE_FAST:
-            EPD_W21_WriteCMD(0X50); // VCOM AND DATA INTERVAL SETTING
-            EPD_W21_WriteDATA(0x29);
-            EPD_W21_WriteDATA(0x07);
-            EPD_W21_WriteCMD(0xE0);
-            EPD_W21_WriteDATA(0x02);
-            EPD_W21_WriteCMD(0xE5);
-            EPD_W21_WriteDATA(0x5A);
+        case UPDATE_FAST:
+            WriteCmd(0X50, screen); // VCOM AND DATA INTERVAL SETTING
+            WriteData(0x29, screen);
+            WriteData(0x07, screen);
+            WriteCmd(0xE0, screen);
+            WriteData(0x02, screen);
+            WriteCmd(0xE5, screen);
+            WriteData(0x5A, screen);
             break;
 
-        case EPD_UPDATE_PARTIAL:
-            EPD_W21_WriteCMD(0xE0);
-            EPD_W21_WriteDATA(0x02);
-            EPD_W21_WriteCMD(0xE5);
-            EPD_W21_WriteDATA(0x6E);
+        case UPDATE_PARTIAL:
+            WriteCmd(0xE0, screen);
+            WriteData(0x02, screen);
+            WriteCmd(0xE5, screen);
+            WriteData(0x6E, screen);
             break;
 
-        case EPD_UPDATE_FULL:
+        case UPDATE_FULL:
         default: // Default case for safety
-            EPD_W21_WriteCMD(0X50); // VCOM AND DATA INTERVAL SETTING
-            EPD_W21_WriteDATA(0x21);
-            EPD_W21_WriteDATA(0x07);
+            WriteCmd(0X50, screen); // VCOM AND DATA INTERVAL SETTING
+            WriteData(0x21, screen);
+            WriteData(0x07, screen);
 
             /* Additional code for 4G
-            EPD_W21_WriteCMD(0xE0);
-            EPD_W21_WriteDATA(0x02);
-            EPD_W21_WriteCMD(0xE5);
-            EPD_W21_WriteDATA(0x5F);
+            WriteCmd(0xE0, screen);
+            WriteData(0x02, screen);
+            WriteCmd(0xE5, screen);
+            WriteData(0x5F, screen);
             */
 
             break;
@@ -109,75 +131,75 @@ void epd_init(int mode) {
 }
 
 // Update
-void epd_update(void) {
-    EPD_W21_WriteCMD(0x12); // DISPLAY update
+void updateDisplay(int screen) {
+    WriteCmd(0x12, screen); // DISPLAY update
     delay(DISPLAY_UPDATE_DELAY);
-    epd_wait_busy();
+    waitBusy(screen);
 }
 
 // Data Transfer
-void epd_fill_screen(const unsigned char *data, unsigned char fill_value, bool fast_update) {
+void fillScreen(const unsigned char *data, unsigned char fill_value, bool fast_update, int screen) {
     unsigned int i;
 
-    EPD_W21_WriteCMD(0x10); // write old data
+    WriteCmd(0x10, screen); // write old data
     for (i = 0; i < EPD_ARRAY; i++) {
-        EPD_W21_WriteDATA(EPD_WHITE);
+        WriteData(EPD_WHITE, screen);
     }
 
-    EPD_W21_WriteCMD(0x13); // write new data
+    WriteCmd(0x13, screen); // write new data
     if (data != NULL) {
         for (i = 0; i < EPD_ARRAY; i++) {
-            EPD_W21_WriteDATA(data[i]);
+            WriteData(data[i], screen);
         }
     } else {
         for (i = 0; i < EPD_ARRAY; i++) {
-            EPD_W21_WriteDATA(fill_value);
+            WriteData(fill_value, screen);
         }
     }
-    epd_update();
+    updateDisplay(screen);
 }
 
-void epd_display(const unsigned char *image) {
-    epd_fill_screen(image, EPD_WHITE, false);
+void displayImage(const unsigned char *image, int screen) {
+    fillScreen(image, EPD_WHITE, false, screen);
 }
 
-void epd_partial_update(unsigned int x_start, unsigned int y_start, const unsigned char *data, unsigned int part_column, unsigned int part_line) {
+void partialUpdate(unsigned int x_start, unsigned int y_start, const unsigned char *data, unsigned int part_column, unsigned int part_line, int screen) {
     unsigned int x_end = x_start + part_line - 1;
     unsigned int y_end = y_start + part_column - 1;
     unsigned int i;
 
-    epd_init(EPD_UPDATE_PARTIAL);
+    initDisplay(UPDATE_PARTIAL, screen);
 
-    EPD_W21_WriteCMD(0x91); // Enter partial mode
-    EPD_W21_WriteCMD(0x90); // Resolution setting
-    EPD_W21_WriteDATA(x_start / 256);
-    EPD_W21_WriteDATA(x_start % 256);
-    EPD_W21_WriteDATA(x_end / 256);
-    EPD_W21_WriteDATA((x_end % 256) - 1);
-    EPD_W21_WriteDATA(y_start / 256);
-    EPD_W21_WriteDATA(y_start % 256);
-    EPD_W21_WriteDATA(y_end / 256);
-    EPD_W21_WriteDATA((y_end % 256) - 1);
-    EPD_W21_WriteDATA(0x01);
+    WriteCmd(0x91, screen); // Enter partial mode
+    WriteCmd(0x90, screen); // Resolution setting
+    WriteData(x_start / 256, screen);
+    WriteData(x_start % 256, screen);
+    WriteData(x_end / 256, screen);
+    WriteData((x_end % 256) - 1, screen);
+    WriteData(y_start / 256, screen);
+    WriteData(y_start % 256, screen);
+    WriteData(y_end / 256, screen);
+    WriteData((y_end % 256) - 1, screen);
+    WriteData(0x01, screen);
 
-    EPD_W21_WriteCMD(0x13); // Write new data
+    WriteCmd(0x13, screen); // Write new data
     for (i = 0; i < part_column * part_line / 8; i++) {
-        EPD_W21_WriteDATA(data[i]);
+        WriteData(data[i], screen);
     }
-    epd_update();
+    updateDisplay(screen);
 }
 
-void epd_partial_update_all(const unsigned char *data) {
-    epd_partial_update(0, 0, data, EPD_HEIGHT, EPD_WIDTH);
+void partialUpdateAll(const unsigned char *data, int screen) {
+    partialUpdate(0, 0, data, EPD_HEIGHT, EPD_WIDTH, screen);
 }
 
-void epd_deep_sleep(void) {
-    EPD_W21_WriteCMD(0X50); // VCOM AND DATA INTERVAL SETTING
-    EPD_W21_WriteDATA(0xf7); // WBmode:VBDF 17|D7 VBDW 97 VBDB 57  WBRmode:VBDF F7 VBDW 77 VBDB 37  VBDR B7
+void deepSleep(int screen) {
+    WriteCmd(0X50, screen); // VCOM AND DATA INTERVAL SETTING
+    WriteData(0xf7, screen); // WBmode:VBDF 17|D7 VBDW 97 VBDB 57  WBRmode:VBDF F7 VBDW 77 VBDB 37  VBDR B7
 
-    EPD_W21_WriteCMD(0X02); // power off
-    epd_wait_busy();
+    WriteCmd(0X02, screen); // power off
+    waitBusy(screen);
     delay(SLEEP_DELAY);
-    EPD_W21_WriteCMD(0X07); // deep sleep
-    EPD_W21_WriteDATA(0xA5);
+    WriteCmd(0X07, screen); // deep sleep
+    WriteData(0xA5, screen);
 }
